e5ac55b4a47aeaab7006a55405c71577a6d7ac28
richardCar <richi-cardozo@hotmail.com>
2018-12-13 07:34:20 -0500

    add method that renderer points every 5 seconds

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,8 @@
     ], function(Map, WebMap, MapView, Graphic, LayerList, FeatureLayer, Legend, Expand, Search, Home) {
 
 
-
+      var pointGraphic, inteval;
+      var points = 0;
       var layer = new FeatureLayer({
         portalItem: {
           id: "de76fa5424774a0c8cbdcc8089404362"
@@ -90,14 +91,17 @@
 
 
       mainView
-      //.when(addPoints);
-        .when(maintainFixedExtent);
-        //.then(addPoints);
+        //.when(addPoints);
+        .when(maintainFixedExtent)
+        //.then()
+        .then(addPoints);
         // .then(disablePopupOnClick)
         // .then(enableHighlightOnPointerMove);
         //.then(disableNavigation)
 
-        setInterval(addPoint(mainView), 6000);
+        function imprimir(){
+        console.log("eyyy");
+        }
 
       function maintainFixedExtent(view) {
         var fixedExtent = view.extent.clone();
@@ -105,141 +109,78 @@
         // when the view size changes
         view.on("resize", function() {
           view.extent = fixedExtent;
-          //addPoints();
         });
         return view;
       }
 
       var highlight = null;
 
+      var delay = ( function() {
+        var timer = 0;
+        return function(callback, ms) {
+            clearTimeout (timer);
+            timer = setTimeout(callback, ms);
+        };
+    })();
+
+    function wait(ms){
+     var start = new Date().getTime();
+     var end = start;
+     while(end < start + ms) {
+       end = new Date().getTime();
+    }
+  }
+
       function addPoints(view){
-        console.log("addPoints");
+         // while (points < 5){
+          //addPoint(view);
+          //delay(addPoint(view),5000);
+          interval = setInterval(addPoint, 5000);
+          //console.log("#point:",points);
+        //  console.log(interval);
+          //points++;
+          //wait(5000);
+          //clearInterval(interval);
+        // }
 
 
+    }
 
-
-      }
-
-      function addPoint(view){
-        for (i = 0; i< 5;i++){
+    function addPoint(){
+       // interval = setInterval(function(){
+        console.log("add one Point");
           var point = {
             type: "point",  // autocasts as new Point()
             longitude: (Math.random() * (-78.99 - (-66.87)) + (-66.87)).toFixed(3) * 1,
             latitude: (Math.random() * (-4.29 - (12.43)) + (12.43)).toFixed(3) * 1
           };
-
           var markerSymbol = {
             type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
             color: [226, 119, 40]
           };
-
           var pointGraphic = new Graphic({
             geometry: point,
             symbol: markerSymbol
           });
-          console.log(pointGraphic);
-          mainView.graphics.add(pointGraphic);
-          console.log("point added");
-        }
-      }
+           // interval = setInterval(function(){
 
-      function enableHighlightOnPointerMove(view) {
-        view.whenLayerView(layer).then(function(layerView) {
-          view.on("pointer-move", function(event) {
-            view.hitTest(event)
-              .then(function(response) {
+            mainView.graphics.add(pointGraphic);
+            console.log("point added");
+            //changeRenderer(view, pointGraphic);
+            //delay(changeRenderer(view, pointGraphic),5000);
 
-                // remove the previous highlight
-                if (highlight) {
-                  highlight.remove();
-                  highlight = null;
-                }
+            //clearInterval(interval);
+       // },5000);
 
-                // if a feature is returned, highlight it
-                // and display its attributes in the popup
-                // if no features are returned, then close the popup
-                var id = null;
+    }
 
-                if (response.results.length) {
-                  var feature = response.results.filter(function(
-                    result) {
+    function changeRenderer(view, pointGraphic){
+      var newMarkerSymbol = {
+        type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+        color: [0, 0, 0]
+      };
+      delay(function(){pointGraphic.symbol = newMarkerSymbol;},2000);
 
-
-                    return result.graphic.layer === layer;
-                  })[0].graphic;
-                  feature.popupTemplate = layer.popupTemplate;
-                  id = feature.attributes.OBJECTID;
-                  highlight = layerView.highlight([id]);
-                  var selectionId = mainView.popup.selectedFeature ?
-                    mainView.popup.selectedFeature.attributes
-                    .OBJECTID : null;
-
-                  if (highlight && (id !== selectionId)) {
-                    mainView.popup.open({
-                      features: [feature],
-                      updateLocationEnabled: true
-                    });
-                  }
-                } else {
-                  if (mainView.popup.visible) {
-                    mainView.popup.close();
-                    mainView.popup.clear();
-                  }
-                }
-              });
-          });
-
-        });
-      }
-
-      // disables all navigation in the view
-      function disableNavigation(view) {
-        view.popup.dockEnabled = true;
-
-        // Removes the zoom action on the popup
-        view.popup.actions = [];
-
-        // stops propagation of default behavior when an event fires
-        function stopEvtPropagation(event) {
-          event.stopPropagation();
-        }
-
-        // disable mouse wheel scroll zooming on the view
-        view.on("mouse-wheel", stopEvtPropagation);
-
-        // disable zooming via double-click on the view
-        view.on("double-click", stopEvtPropagation);
-
-        // disable zooming out via double-click + Control on the view
-        view.on("double-click", ["Control"], stopEvtPropagation);
-
-        // disables pinch-zoom and panning on the view
-        view.on("drag", stopEvtPropagation);
-
-        // disable the view's zoom box to prevent the Shift + drag
-        // and Shift + Control + drag zoom gestures.
-        view.on("drag", ["Shift"], stopEvtPropagation);
-        view.on("drag", ["Shift", "Control"], stopEvtPropagation);
-
-        // prevents zooming and rotation with the indicated keys
-        view.on("key-down", function(event) {
-          var prohibitedKeys = ["+", "-", "_", "=", "a", "d"];
-          var keyPressed = event.key.toLowerCase();
-          if (prohibitedKeys.indexOf(keyPressed) !== -1) {
-            event.stopPropagation();
-          }
-        });
-
-        return view;
-      }
-
-      // prevents the user from opening the popup with click
-
-      function disablePopupOnClick(view) {
-        view.on("click", function(event) {
-          event.stopPropagation();
-        });
-        return view;
-      }
+    }
 
     });
